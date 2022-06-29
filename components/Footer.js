8e606b172835fba3ca786386391f957e63127e25
kamrulcoder <kamrulcoder@gmail.com>
2022-06-30 00:01:47 +0600

    Footer Component Add

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import styles from "../styles/footer.module.css"
+import { Fade } from "react-reveal";
+import emoji from "react-easy-emoji";
 
-const Footer = () => {
-    return (
-        <div>
-            <h2>Footer Components </h2>
-        </div>
-    );
-};
-
-export default Footer;
+export default function Footer() {
+  return (
+    <Fade bottom duration={1000} distance="5px">
+    <div className={styles.footerdiv}>
+      <p className={styles.footertext}>{emoji("Made with ❤️ by Kamrul Hasan")}</p>
+    </div>
+    </Fade>
+  );
+}
