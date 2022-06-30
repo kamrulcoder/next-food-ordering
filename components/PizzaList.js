0ab128829973d114fe73e56a58d5b1a59be0c0e8
kamrulcoder <kamrulcoder@gmail.com>
2022-06-30 22:23:39 +0600

    All Product Get and Show Frontend

--- components/PizzaList.js
+++ components/PizzaList.js
@@ -1,7 +1,7 @@
 import styles from "../styles/PizzaList.module.css";
 import PizzaCard from "./PizzaCard"
 
-const PizzaList = () => {
+const PizzaList = ({pizzList}) => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
@@ -11,14 +11,10 @@ const PizzaList = () => {
         sit amet, consectetur adipiscing elit.
       </p>
       <div className={styles.wrapper}>
-          <PizzaCard/>
-          <PizzaCard/>
-          <PizzaCard/>
-          <PizzaCard/>
-          <PizzaCard/>
-          <PizzaCard/>
-          <PizzaCard/>
-          <PizzaCard/>
+      {pizzList.map(pizza => (
+        <PizzaCard   key={pizza.id}  pizza={pizza}/>
+      ))}
+          
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
+import axios from 'axios'
 import Head from 'next/head'
 import Image from 'next/image'
 import Featured from '../components/Featured'
 import PizzaList from '../components/PizzaList'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({pizzList}) {
   return (
    <>
     <Head>
@@ -13,7 +14,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured/>
-      <PizzaList/>
+      <PizzaList  pizzList={pizzList}/>
       </>
   )
+}
+
+
+export  const getServerSideProps = async () => {
+  const res = await axios.get("http://localhost:3000/api/products")
+  return {
+    props:{
+      pizzList : res.data
+    }
+  }
 }
